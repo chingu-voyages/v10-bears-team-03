c8426232abf854cca6e182bc43f6459162f51419
Canino Developments <ian@thecommittedbug.io>
2019-07-10 15:24:57 -0400

    add routing for AssetList for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
+import AssetListContainer from './components/AssetListContainer';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Landing} />
+          <Route exact path='/assets' component={AssetListContainer} />
         </Switch>
       </BrowserRouter>
     );
