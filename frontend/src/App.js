58d3cf5dd29a6229c1aec38528a388d4f57ec6e3
Canino Developments <ian@thecommittedbug.io>
2019-07-13 05:33:54 -0400

    add routing for assetslistform component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
+import AssetsAndFormContainer from './components/AssetsAndFormContainer';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Landing} />
+          <Route exact path='/form' component={AssetsAndFormContainer} />
         </Switch>
       </BrowserRouter>
     );
