895ba4b7176cd6b5a8ca9327996cde7f3c95f650
Canino Developments <ian@thecommittedbug.io>
2019-07-26 19:58:10 -0400

    refactor responivelayout component

--- frontend/src/components/ResponsiveLayout.jsx
+++ frontend/src/components/ResponsiveLayout.jsx
@@ -1,13 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useWindowDimensions } from '../WindowDimensionsProvider';
 
-const ResponsiveLayout = ({ breakpoint, renderMobile, renderDesktop }) => {
-  const [width, setWidth] = useState(window.innerWidth)
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth)
-    }
-    window.addEventListener(‘resize’, handleResize)
-    return () => { window.removeEventListener(‘resize’, handleResize) }
-  }, [])
-  return (width > breakpoint ? renderDesktop() : renderMobile())
+const ResponsiveLayout = ({ breakPoint, renderMobile, renderDesktop }) => {
+  const { width } = useWindowDimensions()
+  return (width > breakPoint ? renderDesktop() : renderMobile())
 }
+
+export default ResponsiveLayout;
