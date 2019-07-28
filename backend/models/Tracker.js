edbac6123847c5fc74d166c09c084170203f67df
Felix Chan <felixchan29@gmail.com>
2019-07-28 17:32:44 -0400

    condense Tracker model

--- backend/models/Tracker.js
+++ backend/models/Tracker.js
@@ -4,24 +4,12 @@ const Schema = mongoose.Schema;
 
 
 const TrackerSchema = new Schema({
-  name: {
-      type: String
-  },
-  type: {
-      type: String
-  },
-  date_purchased: {
-      type: Date
-  },
-  expire: {
-      type: Number
-  },
-  price: {
-      type: Number
-  },
-  where_purchased: {
-      type: String
-  },
+  name: String,
+  type: String,
+  date_purchased: Date,
+  expire: Number,
+  price:  Number,
+  where_purchased: String,
   UserTrackerGroup: [{ type:Schema.Types.ObjectId, ref:"UserTrackers" }]
 
 });
