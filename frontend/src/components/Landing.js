b769498a9040be7559b5898c83454fa040f7a1cc
Canino Developments <ian@thecommittedbug.io>
2019-07-31 17:13:58 -0400

    make landing page responsive

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -9,7 +9,6 @@ const Landing = () => {
   return (
     <div>
       <div className='landing-container'>
-        <Navbar id='landing-container-navbar' />
         <Logo />
         <button onClick={startGoogleLogin}>Log In With Google</button>
         <button onClick={() => history.push('/emaillogin')}>
