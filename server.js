87898902a5fb4a7fbaa395a5c77e644678f1ad80
Felix Chan <felixchan29@gmail.com>
2019-08-12 20:53:03 -0400

    reformate so heroku can be accepted this

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require("path")
+
 
 // const trackerRoutes = express.Router();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "client", "build")))
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
+
   }
   
 
