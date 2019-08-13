b6a299a733e99a921beb6138dc12b6aacf739ac8
Felix Chan <felixchan29@gmail.com>
2019-08-12 21:16:05 -0400

    more fixing bug

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.use(express.static(path.join(__dirname, "client/build")))
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
